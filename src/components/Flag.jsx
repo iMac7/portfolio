57a52f51c14effd4d399a3a54f6f3eaeb733a7d1
imac <ian.macharia@stream4.tech>
2023-04-02 23:40:21 +0300

    added panels, to fix panel positions

--- src/components/Flag.jsx
+++ src/components/Flag.jsx
@@ -4,7 +4,7 @@ import { useFrame, useLoader } from "@react-three/fiber"
 import {vertexShader, fragmentShader} from '../../public/shaders'
 
 
-export default function Flag() {
+export default function Flag({position}) {
     const flagTexture = useLoader(TextureLoader, '/textures/kenya.png')
 
     const meshRef = useRef()
@@ -16,8 +16,8 @@ export default function Flag() {
 
   return (
     <>
-        <mesh position={[3,15,-15]} ref={meshRef} >
-         <planeGeometry args={[4,2.5,10,10]}/>
+        <mesh position={position} rotation={[0,Math.PI*.2,0]} ref={meshRef} >
+         <planeGeometry args={[15,15,20,20]}/>
          <rawShaderMaterial
           uniforms={{
             uTexture: {value: flagTexture},
--- src/components/Meshes.jsx
+++ src/components/Meshes.jsx
@@ -1,6 +1,6 @@
-import { useRef } from "react"
-import { DoubleSide, TextureLoader, MeshBasicMaterial, Color, MeshStandardMaterial } from "three"
-import { MeshReflectorMaterial, Stars, useGLTF, MeshRefractionMaterial, Image, Text } from "@react-three/drei"
+import { useRef, useState } from "react"
+import { DoubleSide, TextureLoader, MeshBasicMaterial, Color, MeshStandardMaterial, Vector3 } from "three"
+import { MeshReflectorMaterial, Stars, useGLTF, MeshRefractionMaterial, Image, Text, Sparkles } from "@react-three/drei"
 import { useFrame, useLoader } from "@react-three/fiber"
 import Flag from "./Flag"
 
@@ -18,7 +18,6 @@ function Meshes() {
 
   return (
     <>
-
         //Diamond
         <mesh position={[0,0,0]} ref={diamondRef} geometry={nodes.Diamond_1_0.geometry} scale={[5,5,5]}>
             <MeshRefractionMaterial wireframe={false} envMap={worldTexture} toneMapped={false}
@@ -30,14 +29,29 @@ function Meshes() {
         //INTRO PANEL
         <mesh position={[-60,-10,-90]} rotation={[0,Math.PI*.2,0]}>
             <planeGeometry args={[100,150,10,10]}/>
-            <MeshReflectorMaterial
-            color={'black'}
-            // emissive={'red'}
+            {/* <MeshReflectorMaterial
             side={DoubleSide}
-            transparent
-            opacity={.5}
-            blur={.5}
-             />
+            envMapIntensity={0}
+            dithering={true}
+            color={[.05,.05,.05]}
+            roughness={.7}
+            blur={.9}
+            mixBlur={30}
+            mixStrength={20}
+            mixContrast={1}
+            mirror={1}
+            depthScale={.01}
+            minDepthThreshold={.9}
+            maxDepthThreshold={1}
+            depthToBlurRatioBias={.25}
+            reflectorOffset={.2}
+            debug={0}
+            // transparent
+            // opacity={.7}
+            // wireframe={true}
+            /> */}
+            <meshNormalMaterial />
+
         </mesh>
 
         {/* <Image 
@@ -49,13 +63,15 @@ function Meshes() {
          scale={10} 
          onClick={()=>console.log('clicked')} /> */}
 
-        <Text color={new Color(100,1,1)} position={[-60,-10,-88]} fontSize={5} rotation={[0,Math.PI*.2,0]}>
+        <Text color={new Color(50,1,1)} position={[-60,-50,-86]} fontSize={5} rotation={[0,Math.PI*.2,0]}>
             NAME &nbsp;:&nbsp; ME
+            {'\n'}
+            {'\n'}COUNTRY : KENYA
             {'\n'}
             {'\n'}SPECIALTY : WEB DEVELOPMENT
         </Text>
 
-        {/* <Flag /> */}
+        <Flag position={[-40,-25,-86]}/>
 
 
 
@@ -66,94 +82,6 @@ function Meshes() {
          //BESIDE BED
         <mesh position={[130,-10,50]} rotation={[0,Math.PI*-.6,0]}>
           <planeGeometry args={[140,200,10,10]}/>
-          <MeshReflectorMaterial
-          color={'black'}
-          emissive={'black'}
-          side={DoubleSide}
-          transparent
-          opacity={.9}
-            />
-        </mesh>
-
-        <Text color={new Color(100,1,1)} position={[129,-10,50]} rotation={[0,Math.PI*-.6,0]} fontSize={5}>
-            NAME &nbsp;:&nbsp; ME
-            {'\n'}
-            {'\n'}SPECIALTY : WEB DEVELOPMENT
-        </Text>
-
-
-
-
-
-
-
-         //ON BED
-        <mesh position={[-30,-50,130]} rotation={[Math.PI*0.5,Math.PI*0,Math.PI*0.13]}>
-            <planeGeometry args={[130,80,10,10]}/>
-            <MeshReflectorMaterial
-            color={'black'}
-            emissive={'black'}
-            side={DoubleSide}
-            transparent
-            opacity={.9}
-             />
-        </mesh>
-
-        <Text color={new Color(100,1,1)} position={[-30,-49,130]} rotation={[Math.PI*0.5,Math.PI*0,Math.PI*0.13]} fontSize={5}>
-            NAME &nbsp;:&nbsp; ME
-            {'\n'}
-            {'\n'}SPECIALTY : WEB DEVELOPMENT
-        </Text>
-
-
-
-
-
-
-
-
-
-
-
-         //CEILING
-        <mesh position={[0,100,-10]} rotation={[Math.PI*0.5,Math.PI*0,Math.PI*0.3]}>
-            <planeGeometry args={[170,150,10,10]}/>
-            <MeshReflectorMaterial
-            color={'black'}
-            emissive={'black'}
-            side={DoubleSide}
-            transparent
-            opacity={.9}
-             />
-        </mesh>
-
-        <Text color={new Color(100,1,1)} position={[0,99,-10]} rotation={[Math.PI*0.5,Math.PI*0,Math.PI*0.3]} fontSize={5}>
-            NAME &nbsp;:&nbsp; ME
-            {'\n'}
-            {'\n'}SPECIALTY : WEB DEVELOPMENT
-        </Text>
-
-
-
-
-
-        {/* <mesh position={[0,4,0]}>
-        <boxGeometry args={[1, 1, 1]} />
-        <meshStandardMaterial color={[100,1,1]} emissive={'hotpink'} />
-        </mesh> */}
-
-
-
-
-
-
-        {/* <mesh position={[3,1,0]} castShadow receiveShadow >
-          <boxGeometry args={[3,2,4]} />
-          <meshStandardMaterial color={'black'} transparent opacity={.9}/>
-        </mesh> */}
-
-        {/* <mesh castShadow receiveShadow rotation-x={-Math.PI * .5} position={[0,-10,0]}>
-          <planeGeometry args={[30, 30, 10, 10]} />
           <MeshReflectorMaterial
             side={DoubleSide}
             envMapIntensity={0}
@@ -171,18 +99,137 @@ function Meshes() {
             depthToBlurRatioBias={.25}
             reflectorOffset={.2}
             debug={0}
+            transparent
+            opacity={.7}
             // wireframe={true}
+          />
+
+        </mesh>
+
+        <Text color={new Color(0,1,0)} position={[129,-10,50]} rotation={[0,Math.PI*-.6,0]} fontSize={5}>
+            PROFESSIONAL QUALIFICATIONS
+            {'\n'}
+            {'\n'}SPECIALTY : WEB DEVELOPMENT
+        </Text>
+
+
+
+
+
+
+
+         //ON BED
+         <mesh position={[-50,10,130]} rotation={[Math.PI*0,Math.PI*-0.1,Math.PI*0]}>
+            <planeGeometry args={[80,120,10,10]}/>
+            <MeshReflectorMaterial
+            side={DoubleSide}
+            envMapIntensity={0.5}
+            dithering={true}
+            color={'black'}
+            roughness={.7}
+            blur={.5}
+            mixBlur={30}
+            mixStrength={20}
+            mixContrast={1}
+            mirror={1}
+            depthScale={.01}
+            minDepthThreshold={.9}
+            maxDepthThreshold={1}
+            depthToBlurRatioBias={.25}
+            reflectorOffset={.2}
+            debug={0}
+            transparent
+            opacity={.97}
             />
+
+        </mesh>
+
+        <Text color={new Color(1000,1,1)} position={[-50,50,129]} rotation={[Math.PI*0,Math.PI*.9,Math.PI*0]} fontSize={9}>
+            SKILLS
+        </Text>
+        <Text color={new Color(50,1,1)} position={[-50,10,129]} rotation={[Math.PI*0,Math.PI*.9,Math.PI*0]} fontSize={5}>
+            {'\n'}HTML
+            {'\n'}CSS
+            {'\n'}SASS
+            {'\n'}JAVASCRIPT
+            {'\n'}REACT
+            {'\n'}NODE.JS
+            {'\n'}NEXT.JS
+            {'\n'}SQL
+            {'\n'}MONGODB
+            {'\n'}WEB3
+        </Text>
+
+
+
+
+
+
+
+
+
+
+
+         //CEILING
+        {/* <mesh position={[0,100,-10]} rotation={[Math.PI*0.5,Math.PI*0,Math.PI*0.3]}>
+            <planeGeometry args={[130,150,10,10]}/>
+            <MeshReflectorMaterial
+            side={DoubleSide}
+            envMapIntensity={0}
+            dithering={true}
+            color={[.05,.05,.05]}
+            roughness={.7}
+            blur={[1000, 400]}
+            mixBlur={30}
+            mixStrength={20}
+            mixContrast={1}
+            mirror={1}
+            depthScale={.01}
+            minDepthThreshold={.9}
+            maxDepthThreshold={1}
+            depthToBlurRatioBias={.25}
+            reflectorOffset={.2}
+            debug={0}
+            />
+
         </mesh> */}
 
-        {/* <Sparkles
-        count={1000}
+        
+        <Text color={new Color(100,0,1)} emissive={'purple'} position={[-70,99,20]} rotation={[Math.PI*0.5,Math.PI*0,Math.PI*0.3]} fontSize={8}>
+          OTHER INTERESTS
+        </Text>
+
+        <Text color={new Color(100,0,1)} emissive={'purple'} position={[-30,99,5]} rotation={[Math.PI*0.5,Math.PI*0,Math.PI*0.3]} fontSize={5}>
+            CLOUD
+            {'\n'}{'\n'}HARDWARE
+            {'\n'}{'\n'}SECURITY
+            {'\n'}{'\n'}GAMING
+            {'\n'}{'\n'}FOOTBALL
+            {'\n'}{'\n'}TRAVELING
+
+        </Text>
+
+
+
+
+
+
+
+
+
+        {/* <mesh position={[3,1,0]} castShadow receiveShadow >
+          <boxGeometry args={[3,2,4]} />
+          <meshStandardMaterial color={'black'} transparent opacity={.9}/>
+        </mesh> */}
+
+        <Sparkles
+        count={10}
         speed={1}
         opacity={10}
-        size={2}
+        size={4}
         scale={100}
         color={[255,255,0]}
-         /> */}
+         />
 
     </>
   )
