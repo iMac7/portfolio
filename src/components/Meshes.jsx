7c88813e180bde3051a5e7e41db282ebdc1b450d
imac <ian.macharia@stream4.tech>
2023-04-04 20:06:28 +0300

    contact page done

--- src/components/Meshes.jsx
+++ src/components/Meshes.jsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import { DoubleSide, TextureLoader, MeshBasicMaterial, Color, MeshStandardMaterial, Vector3, BackSide } from "three"
 import { MeshReflectorMaterial, Stars, useGLTF, MeshRefractionMaterial, Image, Text, Sparkles, MeshTransmissionMaterial } from "@react-three/drei"
 import { useFrame, useLoader } from "@react-three/fiber"
@@ -10,9 +10,22 @@ function Meshes() {
     const { nodes } = useGLTF('/models/diamond.glb')
     const worldTexture = useLoader(TextureLoader, '/envMaps/room.jpg')
 
+    
     // useFrame(state => {
-    //     diamondRef.current.rotation.y += .01
-    // })
+      //     diamondRef.current.rotation.y += .01
+      // })
+      
+    const [name, setName] = useState('me')
+    useEffect(() => {
+      const timeOut = setTimeout(() => {
+        setName('this is the set name')
+      }, 3000);
+    
+      return () => {
+        clearTimeout(timeOut)
+      }
+    }, [])
+    
 
 
   return (
@@ -29,20 +42,13 @@ function Meshes() {
         <mesh position={[-60,-10,-90]} rotation={[0,Math.PI*.2,0]}>
             <planeGeometry args={[120,150,10,10]}/>
             <MeshTransmissionMaterial
-            // color={[0,50,1000]}
-            color={[0,0,.1]}
+            color={[0,0,0]}
             roughness={.9}
             thickness={3}
-            emissive={'black'} emissiveIntensity={2} toneMapped={false}
-            transparent opacity={.8} side={DoubleSide}
+            emissive={'black'} toneMapped={false}
+            transparent opacity={.1} side={DoubleSide}
             />
         </mesh>
-
-        {/* <mesh position={[-57,-10,-80]} rotation={[0,Math.PI*.2,0]}>
-            <planeGeometry args={[115,140,10,10]}/>
-            <meshBasicMaterial color={'black'} />
-        </mesh> */}
-
 
         <Image 
          url="/images/me.png"
@@ -52,24 +58,22 @@ function Meshes() {
          onClick={()=>console.log('clicked')} />
 
         <Text 
-        color={new Color(10,100,10)}
-         position={[-60,-50,-80]} fontSize={5} rotation={[0,Math.PI*.2,0]}
-        material={new MeshStandardMaterial()}
+        color={[.8,.8,.8]}
+         position={[-60,-50,-80]} fontSize={6} rotation={[0,Math.PI*.2,0]}
+        material={new MeshStandardMaterial({toneMapped:false})}
         >
-            NAME &nbsp;:&nbsp; IAN MACHARIA
-            {'\n'}
+            NAME &nbsp;:&nbsp; {name}
+            {'\n'}{'\n'}
             {'\n'}SPECIALTY : WEB DEVELOPMENT
-            {'\n'}
-            {'\n'}LANGUAGES : ENGLISH , SWAHILI , GERMAN
         </Text>
 
-        {/* <Flag position={[-40,-25,-86]}/> */}
 
 
 
 
 
         //CONTACT DETAILS
+
         <mesh position={[50,-10,-70]} rotation={[0,Math.PI*-.2,0]}>
           <planeGeometry args={[80,30,10,10]}/>
           <meshStandardMaterial
@@ -92,6 +96,7 @@ function Meshes() {
 
 
         //PROJECTS
+
         <mesh position={[110,40,40]} rotation={[0,Math.PI*-.6,0]}>
           <planeGeometry args={[130,40,10,10]}/>
           <meshStandardMaterial
@@ -118,11 +123,11 @@ function Meshes() {
         //SKILLS
 
         <mesh position={[-47,10,124]} rotation={[Math.PI*0,Math.PI*-0.1,Math.PI*0]}>
-            <planeGeometry args={[75,115,10,10]}/>
-            <meshStandardMaterial color={[0,.1,9]} toneMapped={false} transparent opacity={.9} side={DoubleSide}  />
+            <planeGeometry args={[75,115,100,100]}/>
+            <meshStandardMaterial color={[0,.1,9]} toneMapped={false} transparent opacity={.9} side={DoubleSide} wireframe={true}  />
         </mesh>
 
-        <Text color={new Color(1000,0,0)} position={[-50,50,120]} rotation={[Math.PI*0,Math.PI*.9,Math.PI*0]} fontSize={9}>
+        <Text position={[-50,50,120]} rotation={[Math.PI*0,Math.PI*.9,Math.PI*0]} fontSize={9}>
             SKILLS
         </Text>
         {/* {new Color(100,1,1)} */}
@@ -151,35 +156,12 @@ function Meshes() {
 
 
 
-         //INTERESTS
-        {/* <mesh position={[0,100,-10]} rotation={[Math.PI*0.5,Math.PI*0,Math.PI*0.3]}>
-            <planeGeometry args={[130,150,10,10]}/>
-            <MeshReflectorMaterial
-            side={DoubleSide}
-            envMapIntensity={0}
-            dithering={true}
-            color={[.05,.05,.05]}
-            roughness={.7}
-            blur={[1000, 400]}
-            mixBlur={30}
-            mixStrength={20}
-            mixContrast={1}
-            mirror={1}
-            depthScale={.01}
-            minDepthThreshold={.9}
-            maxDepthThreshold={1}
-            depthToBlurRatioBias={.25}
-            reflectorOffset={.2}
-            debug={0}
-            />
-
-        </mesh> */}
-
+        //INTERESTS
+         
         <Image 
         url="/images/arsenal.png"
         position={[0,100,-10]} rotation={[Math.PI*0.5,Math.PI*0,Math.PI*0.3]}
         scale={91}
-        // color={'darkgray'}
         grayscale={.8}
         transparent
         opacity={.2}
@@ -204,15 +186,6 @@ function Meshes() {
 
 
 
-
-
-
-
-
-        {/* <mesh position={[3,1,0]} castShadow receiveShadow >
-          <boxGeometry args={[3,2,4]} />
-          <meshStandardMaterial color={'black'} transparent opacity={.9}/>
-        </mesh> */}
 
         <Sparkles
         count={10}
---
+++ src/pages/contacts.js
@@ -0,0 +1,10 @@
+import Contacts from '@/components/Contacts/Contacts'
+import React from 'react'
+
+function contacts() {
+  return (
+    <Contacts />
+  )
+}
+
+export default contacts
