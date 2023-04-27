d6a386347571341ba197f7ec559bdc9557176729
imac <ian.macharia@stream4.tech>
2023-04-27 18:44:13 +0300

    removed more comments

--- src/components/Cameras.jsx
+++ src/components/Cameras.jsx
@@ -2,13 +2,12 @@ import { PerspectiveCamera } from "@react-three/drei"
 import { useFrame, useThree } from "@react-three/fiber"
 import { useEffect, useState } from "react"
 
+//will support from 300px+ device widths, any less and that's not a smartphone lmao
 function Cameras() {
   const [zoom, setZoom] = useState(1)
 
   useEffect(() => {
-    // console.log(window.innerWidth, window.innerHeight)
     if(window.innerWidth < 1000){
-      //because the site will support from 300px+ device widths, any less and that's not a smartphone lmao
       setZoom((window.innerWidth/700)*.5 + .5)
     }
     
--- src/components/Effects.jsx
+++ src/components/Effects.jsx
@@ -26,22 +26,22 @@ export default function Effects() {
       <EffectComposer multisampling={0}>
 
         <Bloom
-          intensity={1.0} // The bloom intensity.
-          blurPass={undefined} // A blur pass.
-          width={Resizer.AUTO_SIZE} // render width
-          height={Resizer.AUTO_SIZE} // render height
-          kernelSize={KernelSize.MEDIUM} // blur kernel size
-          luminanceThreshold={1} // luminance threshold. Raise this value to mask out darker elements in the scene.
-          luminanceSmoothing={0.025} // smoothness of the luminance threshold. Range is [0, 1]
+          intensity={1.0}
+          blurPass={undefined}
+          width={Resizer.AUTO_SIZE}
+          height={Resizer.AUTO_SIZE}
+          kernelSize={KernelSize.MEDIUM}
+          luminanceThreshold={1}
+          luminanceSmoothing={0.025}
         />
 
         <Glitch
-        delay={[.5, 1]} // min and max glitch delay
-        duration={[0.1, 0.5]} // min and max glitch duration
-        strength={[0.1, .3]} // min and max glitch strength
-        mode={GlitchMode.SPORADIC} // glitch mode
-        active={active} // turn on/off the effect (switches between "mode" prop and GlitchMode.DISABLED)
-        ratio={0.85} // Threshold for strong glitches, 0 - no weak glitches, 1 - no strong glitches.
+        delay={[.5, 1]}
+        duration={[0.1, 0.5]}
+        strength={[0.1, .3]}
+        mode={GlitchMode.SPORADIC}
+        active={active}
+        ratio={0.85}
         chromaticAberrationOffset={new Vector2(.5, .5)}
         columns={.1}
         />
